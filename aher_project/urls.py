dc93413a5c081786567ce81be50d8f6e2d40d5ee
csmith-he <chantelle.smith@HistoricEngland.org.uk>
2025-02-27 16:09:14 +0000

    Add AI API to return possible geometry

--- aher_project/urls.py
+++ aher_project/urls.py
@@ -2,12 +2,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls.i18n import i18n_patterns
 from django.urls import include, path
+from aher_project.views.ai import AIAPIView
 
 # COPIED FROM ./arches_her/docker/aher_project/docker/urls.py
 
 urlpatterns = [
     path('', include('arches.urls')),
    path("", include("arches_her.urls")),
+   path('aiapi/', AIAPIView.as_view(), name='ai-api'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # if settings.SHOW_LANGUAGE_SWITCH is True:
---
+++ aher_project/views/ai.py
@@ -0,0 +1,51 @@
+from django.views.generic import View
+from django.http import JsonResponse
+import requests
+import json
+
+class AIAPIView(View):
+    
+    def get(self, request):
+        response = self.geometry_return("Camden")
+        return JsonResponse({"message": response}, status=200)
+
+    def post(self, request):
+        return JsonResponse({"message": "This is a blank API view"}, status=200)
+    
+    def geometry_return (self,name_to_search):
+        
+        #Featureservice numbers relate to the following datasets:
+        # 4 - Ceremonial Counties
+        # 5 - Districts, Metropolitan districts, London boroughs, Unitary authorities
+        # 6 - Civil parishes and communities
+        # 7 - Wards
+        # 9 - Counties
+        # 11 - Westminster constituencies
+        
+        featureservice_numbers = [4, 5, 6, 7, 9, 11]
+
+        # Base URL for the ArcGIS REST API for OS Open Boundary Line
+        base_url = "https://services.arcgis.com/qHLhLQrcvEnxjtPr/ArcGIS/rest/services/OS_OpenBoundaryLine/FeatureServer/{}/query?where=NAME LIKE '%25" + name_to_search + "%25'&outFields=NAME,DESCRIPTIO,&returnGeometry=true&returnIdsOnly=false&f=geojson"
+        
+        
+        results = []
+        for number in featureservice_numbers:
+            current_url = base_url.format(number)
+            response = requests.get(current_url)
+            
+            if response.status_code == 200:
+                # Parse the JSON response
+                data = response.json()
+
+                if len(data["features"]) > 0:
+                    for feature in data["features"]:
+                        feature_data = {}
+                        feature_data["geometry"] = feature["geometry"]
+                        feature_data["name"]= feature["properties"]["NAME"] 
+                        feature_data["description"] = feature["properties"]["DESCRIPTIO"]
+                        results.append(feature_data)
+            else:
+                print(f"Failed to retrieve data: {response.status_code}")
+                
+        if len(results) > 0:
+            return results
